778024cb071af40660c5be09adf3a460c75ef4ea
n1rjal <nirjalpaudel54312@gmail.com>
2024-10-16 22:17:32 +0545

    feat(setup): gh workflows is setup

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM python:3.11-alpine
-WORKDIR /app
-COPY ./scripts/entrypoint.sh /scripts/entrypoint.sh
+ENV PYTHONUNBUFFERED=1
 RUN apk update --no-cache \
     && apk add build-base postgresql-dev libpq --no-cache --virtual .build-deps \
     && pip install --no-cache-dir --upgrade pip
+WORKDIR /app
+COPY ./scripts/entrypoint.sh /scripts/entrypoint.sh
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install -r --no-cache-dir requirements.txt
 RUN apk del .build-deps
 COPY . .
 EXPOSE 8000
--- core/urls.py
+++ core/urls.py
@@ -20,16 +20,13 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from todo.urls import urlpatterns as todo_urls
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Djate Api",
         default_version="v1",
         description="Djate API",
-    ),
-    public=True,
-    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+    ),                                                               public=True,permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
 )
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -38,7 +35,11 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
     path(
         "api/v1/",
         include(
