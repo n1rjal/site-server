efbe2229006ac68fb7c030239f6cea05a75c4020
n1rjal <nirjalpaudel54312@gmail.com>
2024-10-16 23:52:40 +0545

    fix: update postgres to postgresql

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgres",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
